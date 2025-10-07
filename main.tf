76aab06f2a56f3a8e688a68af1c16beeedb18240
manognamukthinuthalapati <92076282+manognamukthinuthalapati@users.noreply.github.com>
2025-10-07 19:17:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,5 @@
 # This is Terraform configuration file post few classes
-variable "environments" {
- description = "Define all environments"
-  type = map(object({
-    instance_type = string
-    location      = string
-  }))
-}
-variable "env"{
-description = "Environment to create/destroy (dev/prod/qa)"
-  type        = string
-}
-variable "instance_specifications"{
- description = "Instance specifications (AMI & subnet)"
-  type = object({
-    ami    = string
-    subnet = string
-  })
-}
+
 provider "aws"{
 region = var.environments[var.env].location
 }
